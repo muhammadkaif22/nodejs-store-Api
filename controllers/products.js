74368c6e84781c697f85453c00b0182abbc3a045
Xcoderx <xcoder304@gmail.com>
2022-07-09 01:37:20 +0500

    now user can selete by feilds and add the pagination

--- controllers/products.js
+++ controllers/products.js
@@ -5,10 +5,8 @@ const asyncWapper = require("../middlewares/asyncWapper");
 // ===================> CRUD Operations <====================
 
 const GetAllProducts = asyncWapper(async (req, res) => {
-  // sorting
-
   // querys
-  const { featured, company, search_query, sortby } = req.query;
+  const { featured, company, search_query, sortby, selecteFld } = req.query;
   let queryObj = {};
 
   if (featured) queryObj.featured = featured === "true" ? true : false;
@@ -18,22 +16,31 @@ const GetAllProducts = asyncWapper(async (req, res) => {
   // search
   if (search_query) queryObj.name = { $regex: search_query };
 
-  console.log(req.query);
-  console.log(queryObj);
-
   const fetchData = ProductsModal.find(queryObj);
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  fetchData.limit(limit).skip(skip);
 
   // sorting the data
   if (sortby) {
     const sortQuerys = sortby.split(",").join(" ");
     fetchData.sort(sortQuerys);
   }
+  // selecte by Flds
+  if (selecteFld) {
+    const FillterdFld = selecteFld.split(",").join(" ");
+    fetchData.select(FillterdFld);
+  }
   const action = await fetchData;
 
   if (action.length !== 0) {
-    res
-      .status(200)
-      .json({ msg: "all products fetched successfully", data: action });
+    res.status(200).json({
+      msg: "all products fetched successfully",
+      dataLength: action.length,
+      data: action,
+    });
   } else {
     res.status(200).json({ msg: "no record founded" });
   }
